9da84dbd4552229bbd46e900a0650ab63604580e
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2022-11-10 01:46:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -576,7 +576,18 @@ grep -qE "^ *@reboot root bash /root/tun.sh >/dev/null 2>&1" /etc/crontab || ech
 green "重启VPS自动开启TUN守护功能已启动"
 fi
 fi
-fi	
+fi
+systemctl stop firewalld.service >/dev/null 2>&1
+systemctl disable firewalld.service >/dev/null 2>&1
+setenforce 0 >/dev/null 2>&1
+ufw disable >/dev/null 2>&1
+iptables -P INPUT ACCEPT >/dev/null 2>&1
+iptables -P FORWARD ACCEPT >/dev/null 2>&1
+iptables -P OUTPUT ACCEPT >/dev/null 2>&1
+iptables -t mangle -F >/dev/null 2>&1
+iptables -F >/dev/null 2>&1
+iptables -X >/dev/null 2>&1
+netfilter-persistent save >/dev/null 2>&1
 if [[ -n $(apachectl -v 2>/dev/null) ]]; then
 systemctl stop httpd.service >/dev/null 2>&1
 systemctl disable httpd.service >/dev/null 2>&1
